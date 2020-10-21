ac6cf48f4d99b654478d32644904a876c1f21d06
Yang-yufeng <542796637@qq.com>
2020-10-21 18:08:07 +0800

    初始导入

---
+++ src/test/java/com/yufeng/activiti/ActivitiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yufeng.activiti;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
